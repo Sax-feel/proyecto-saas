b90f829ddfde293cfb5af01e7e2c9c2c0ec16290
Jnyarz <alberto.10065864@gmail.com>
2026-01-13 05:10:29 -0400

    loginFront

--- frontend/src/app/login/page.js
+++ frontend/src/app/login/page.js
@@ -1,4 +1,3 @@
-// src/app/login/page.js
 "use client";
 
 import { useState } from "react";
@@ -12,42 +11,51 @@ export default function LoginPage() {
   const router = useRouter();
 
   const handleLogin = async (e) => {
-  e.preventDefault();
-  setError("");
+    e.preventDefault();
+    setError("");
 
-  try {
-    const res = await fetch("http://localhost:8000/api/login/", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email, password }),
-    });
+    try {
+      const res = await fetch("http://localhost:8000/api/auth/login/", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      });
 
-    const data = await res.json();
+      const data = await res.json();
 
-    if (!res.ok) {
-      setError("Credenciales inválidas");
-      return;
+      if (!res.ok) {
+        setError("Credenciales inválidas");
+        return;
+      }
+
+      localStorage.setItem("access", data.tokens.access);
+      localStorage.setItem("refresh", data.tokens.refresh);
+
+      // DATOS DE USUARIO
+      const rol = data.user.rol.toLowerCase();
+      const emailUser = data.user.email;
+
+      localStorage.setItem("rol", rol);
+      localStorage.setItem("user_email", emailUser);
+
+      // REDIRECCIÓN POR ROL
+      if (rol === "admin") {
+        router.push("/admin");
+      } else if (rol === "cliente") {
+        router.push("/cliente");
+      } else if (rol === "admin_empresa") {
+        router.push("/usuario_empresa");
+      } else if (rol === "vendedor") {
+        router.push("/vendedor");
+      } else {
+        console.warn("Rol no reconocido:", rol);
+        router.push("/login");
+      }
+
+    } catch (err) {
+      setError("Error de conexión con el servidor");
     }
-
-    // Guardar datos de sesión
-    localStorage.setItem("access", data.access);
-    localStorage.setItem("rol", data.rol);
-
-    // REDIRECCIÓN POR ROL
-    if (data.rol === "ADMIN") {
-      router.push("/admin");
-    } else if (data.rol === "CLIENTE") {
-      router.push("/cliente");
-    } else if (data.rol === "ADMIN_EMPRESA") {
-      router.push("/empresa");
-    } else {
-      router.push("/login");
-    }
-
-  } catch (err) {
-    setError("Error de conexión");
-  }
-};
+  };
 
   return (
     <div className={styles.container}>

721e02cabc622e6ab252354950e2a9afc713f0b9
Sax-feel <chingalait@gmail.com>
2026-01-21 22:02:31 -0400

    delete-ventas

--- backend/ventas/urls.py
+++ backend/ventas/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('realizar-compra/', views.RealizarCompraView.as_view(), name='realizar-compra'),
     path('mis-compras/', views.HistorialComprasClienteView.as_view(), name='mis-compras'),
     path('listar-ventas/', views.ListaVentasVendedorView.as_view(), name='mis-compras'),
+    path('<int:id_venta>/eliminar/', views.EliminarVentaView.as_view(), name='eliminar-venta'),
 ]
